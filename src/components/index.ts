88f3896007306ee2d63422d61caae8062eba637a
Joseph Hale <me@jhale.dev>
2026-01-24 21:19:51 -0700

    refactor: create dedicated components/index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+// Copyright (c) 2026 Joseph Hale
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+export { default as Button, type ButtonProps } from './Button';
+export { default as Card, type CardProps } from './Card';
+export { default as Dialog, type DialogProps } from './Dialog';
+export { default as Divider } from './Divider';
+export { default as Row, type RowProps } from './Row';
+export { default as Text, type TextProps } from './Text';
+export { default as TextInput, type TextInputProps } from './TextInput';
